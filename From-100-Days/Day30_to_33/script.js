f17f575fb5a92ab8d9192ea1f1a4d4d15dc41d4c
Junaeid18 <mdjunaeid98@gmail.com>
2022-03-08 05:14:45 +0600

    script.js added and all scripts shifted

---
+++ From-100-Days/Day30_to_33/script.js
@@ -0,0 +1,9 @@
+let recallText = "Bondhu onk por abr fire ashlam re, niye ne amare vai";
+alert(recallText);
+recallText = "Khaise kos ki mama";
+alert(recallText);
+recallText = "Bepar na aay koley aay beta, chill bro";
+let age = 32;
+alert(age);
+let msg = "script je arek page a ansi kaj hoise ni mama";
+alert(msg);
