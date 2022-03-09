89e1c823edaf3bd0667c6c24a0e40829c3542fd3
Junaeid18 <mdjunaeid98@gmail.com>
2022-03-09 06:16:12 +0600

    simple math opration done

--- From-100-Days/Day30_to_33/operation.js
+++ From-100-Days/Day30_to_33/operation.js
@@ -3,3 +3,17 @@ console.log(10 - 4);
 console.log(10 * 4);
 console.log(10 / 4);
 console.log(10 % 4);
+
+let ans = 10 * 5;
+ans++;
+console.log(ans);
+ans--;
+console.log(ans);
+ans += 5;
+console.log(ans);
+ans -= 5;
+console.log(ans);
+ans /= 5;
+console.log(ans);
+ans *= 5;
+console.log(ans);
