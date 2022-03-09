64c850ed729574ccbe35451fcf3375ae383b6d33
Junaeid18 <mdjunaeid98@gmail.com>
2022-03-09 14:27:48 +0600

    querySelector & querySelectorAll understood

--- From-100-Days/Day33_to_37/script.js
+++ From-100-Days/Day33_to_37/script.js
@@ -1,4 +1,11 @@
 /* console.log(window); */
 //console.dir(document);
 
-document.body.children[1].children[0].href = "http://google.com";
+//document.body.children[1].children[0].href = "http://google.com";
+
+/* let link = document.getElementById("weblink");
+link.href = "https://google.com";
+ */
+
+let link = document.querySelector("a");
+link.href = "https://google.com";
