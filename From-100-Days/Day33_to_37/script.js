890724d40a599186e4735d2b34a6fb5688c133e7
Junaeid18 <mdjunaeid98@gmail.com>
2022-03-10 04:08:01 +0600

    some dom manipulation done for task

--- From-100-Days/Day33_to_37/script.js
+++ From-100-Days/Day33_to_37/script.js
@@ -1,5 +1,5 @@
 /* console.log(window); */
-//console.dir(document);
+console.dir(document);
 
 //document.body.children[1].children[0].href = "http://google.com";
 
@@ -7,5 +7,20 @@
 link.href = "https://google.com";
  */
 
+/* 
 let link = document.querySelector("a");
 link.href = "https://google.com";
+ */
+let header1 = document.body.children[0];
+console.log(header1);
+let parent = header1.parentNode;
+console.log(parent);
+let sibling = header1.parentNode.children[1];
+console.log(sibling);
+
+let header2 = document.getElementById("hselect");
+console.log(header2);
+let para = document.querySelector("p");
+console.log(para);
+
+para.textContent = "Hoye gese change bro!!!!!!!!!!!";
