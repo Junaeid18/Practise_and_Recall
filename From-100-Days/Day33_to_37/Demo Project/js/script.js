344a274db4f59b2fdfe4cb8a548560f247237a5d
Junaeid18 <mdjunaeid98@gmail.com>
2022-03-10 11:39:05 +0600

    Demo project done by me

--- From-100-Days/Day33_to_37/Demo Project/js/script.js
+++ From-100-Days/Day33_to_37/Demo Project/js/script.js
@@ -1,1 +1,11 @@
-let string = 
+let inputBox = document.querySelector("input");
+
+function changeTarget(event) {
+    let string = event.target.value;
+    let stringLength = string.length;
+    let num = 60 - stringLength;
+    document.getElementById("targetNumber").innerHTML = num;
+    console.log(stringLength);
+}
+
+inputBox.addEventListener("input", changeTarget);
