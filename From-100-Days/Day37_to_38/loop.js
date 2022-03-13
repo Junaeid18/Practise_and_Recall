8c4d14c1af72e42fc1c9e22b3e47f4ca723c49b8
Junaeid18 <mdjunaeid98@gmail.com>
2022-03-13 23:11:57 +0600

    for of and for in learned

--- From-100-Days/Day37_to_38/loop.js
+++ From-100-Days/Day37_to_38/loop.js
@@ -3,3 +3,13 @@
 for (let i = 0; i < 10; i++) {
     console.log(i);
 }
+
+const users = ["Junaeid", "Nipa", "Jubaeir"];
+for (const person of users) {
+    console.log(person);
+}
+
+const human = { name: "Junaeid", gender: "male", age: 20 };
+for (const property in human) {
+    console.log(human[property]);
+}
