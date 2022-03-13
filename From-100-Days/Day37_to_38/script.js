da9f1e6af0cc1cf64bf0614c0d35aee87eed0499
Junaeid18 <mdjunaeid98@gmail.com>
2022-03-13 14:50:19 +0600

    removing class using js learned and practiced

--- From-100-Days/Day37_to_38/script.js
+++ From-100-Days/Day37_to_38/script.js
@@ -1,5 +1,8 @@
 const val = 10;
 if (val === 10) {
     const var1 = document.querySelector("h1");
+    const var2 = document.getElementById("btn2");
+    var2.classList.remove("prop");
+    var2.classList.add("prop2");
     var1.remove();
 }
