559f79f5388c3c66813c51efe95378971d86aed2
Junaeid18 <mdjunaeid98@gmail.com>
2022-03-14 13:38:35 +0600

    task-2 done in LoopsInAction

--- From-100-Days/Day37_to_39/loops-in-action/script.js
+++ From-100-Days/Day37_to_39/loops-in-action/script.js
@@ -1,3 +1,5 @@
+//task-1
+
 const sumTheSeries = document.getElementById("seriesSum");
 function doTheSeriesSum() {
     const input = document.getElementById("user-number");
@@ -13,3 +15,16 @@ function doTheSeriesSum() {
     res.style.display = "block";
 }
 sumTheSeries.addEventListener("click", doTheSeriesSum);
+
+//task-2
+
+const btn = document.querySelector("#highlight-links button");
+//console.dir(btn);
+function highlightLinks() {
+    const anchor = document.querySelectorAll("#highlight-links a");
+    //console.dir(anchor);
+    for (const anchorElements of anchor) {
+        anchorElements.classList.add("highlight");
+    }
+}
+btn.addEventListener("click", highlightLinks);
