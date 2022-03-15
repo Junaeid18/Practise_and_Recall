231683fb67817ade3716d1ab8f52cefe4d10eb58
Junaeid18 <mdjunaeid98@gmail.com>
2022-03-15 23:49:59 +0600

    js done for showing overlays and backdrop

--- From-100-Days/Day40_to_44/TicTacToe_project/js/config.js
+++ From-100-Days/Day40_to_44/TicTacToe_project/js/config.js
@@ -1,1 +1,9 @@
-function editPlayer() {}
+function editPlayer() {
+    console.log("here");
+    configOverlayElement.classList.add("enabled");
+    backDropElement.classList.add("enabled");
+    // Can't do backDrop using this way bcz it has no class where the
+    // display property is initialized, so when we are adding enabled
+    // class it is overwritten by it's default css which display none.
+    //backDropElement.style.display = "block";
+}
