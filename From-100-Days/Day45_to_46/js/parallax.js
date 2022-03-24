c66c5e70ec222f0bf2e6275740e88243de787768
Junaeid18 <mdjunaeid98@gmail.com>
2022-03-24 23:44:59 +0600

    Day 45 done and 46 a little bit

--- From-100-Days/Day45_to_46/js/parallax.js
+++ From-100-Days/Day45_to_46/js/parallax.js
@@ -1,6 +1,6 @@
 const element = document.querySelector("#imgFix img");
 new simpleParallax(element, {
-    scale: 1.6,
+    scale: 1.2,
     transition: "cubic-bezier(0,0,0,1)",
     overflow: true,
 });
