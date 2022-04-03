7445d7e7c14a222e8147d9c5f7aed8315187834c
Junaeid18 <mdjunaeid98@gmail.com>
2022-04-03 19:51:18 +0600

    Day 47 finished with basic node initialization

--- From-100-Days/Day46_to_47/app.js
+++ From-100-Days/Day46_to_47/app.js
@@ -4,8 +4,15 @@ console.log(userName);
 const http = require("http");
 
 function myFunc(request, response) {
-    response.statusCode = 200;
-    response.end("<h1> Junaeid </h1>");
+    if (request.url === "/time") {
+        response.statusCode = 200;
+        response.end("<h1>" + new Date() + "</h1>");
+    } else if (request.url === "/") {
+        response.statusCode = 200;
+        response.end("<h1> Junaeid </h1>");
+    } else {
+        response.statusCode = 404;
+    }
 }
 
 const server = http.createServer(myFunc);
