55ec95829aa147b476be62d43721f2ae9cf8407d
Junaeid18 <mdjunaeid98@gmail.com>
2022-04-05 05:05:47 +0600

    userData added and user input saved in userData.json file

--- From-100-Days/Day48_to_49/app.js
+++ From-100-Days/Day48_to_49/app.js
@@ -1,4 +1,7 @@
+const path = require("path");
+const fs = require("fs");
 const exp = require("express");
+const { json } = require("express/lib/response");
 const app = exp();
 
 app.use(exp.urlencoded({ extended: false }));
@@ -16,6 +19,16 @@ app.get("/", function (req, res) {
 
 app.post("/user", function (req, res) {
     const userName = req.body.username;
+
+    const filePath = path.join(__dirname, "data", "userData.json");
+    const readData = fs.readFileSync(filePath);
+
+    const existingUser = JSON.parse(readData);
+    existingUser.push(userName);
+
+    const newInsert = JSON.stringify(existingUser);
+    fs.writeFileSync(filePath, newInsert);
+
     res.send("<h1>" + userName + "</h1>");
     console.log(userName);
 });
